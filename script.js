96dd298e70c1d4526576c99ba72bbf5cbfef777d
Sergei-Sorokin86 <ssa90084@gmail.com>
2024-11-30 22:33:19 +0900

    Create code js

---
+++ script.js
@@ -0,0 +1,59 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+let iPhonebirthdayyear = 2007;
+alert(`Первый iPhone выпущен в ${iPhonebirthdayyear} году`);
+
+let namecreatorJavaScript = 'Брендан Эйх';
+alert(`Создатель Java Script ${namecreatorJavaScript}.`);
+
+let b = 10;
+let c = 2;
+let sum = b + c;
+let subtraction = b - c;
+let multiple = b * c;
+let divide = b / c;
+alert(`Сумма b и с: ${sum}`);
+alert(`Разность b и c: ${subtraction}`);
+alert(`Произведение b и с: ${multiple}`);
+alert(`Частное b и с: ${divide}`);
+
+let d = 2;
+let result = 2**5;
+alert(result);
+
+a = 9;
+b = 2;
+let reminder = a % b;
+alert(reminder);
+
+let num = 1;
+num += 5;
+alert(num);
+num -= 3;
+alert(num);
+num *= 7;
+alert(num);
+num /= 3;
+alert(num);
+num++;
+alert(num);
+num--;
+alert(num);
+
+let age = prompt(`Сколько Вам лет?`);
+alert(age);
+
+const user = {
+    name: 'Имя',
+    age:Number (3),
+    isAdmin: Boolean(true)
+};
+console.log(user['isAdmin']);
+console.log(user['name']);
+
+let question = prompt('Как тебя зовут?');
+alert(`Привет, ${question}!`);
+
